a3225f1287883477d57924cf4ae37238ba8d8fab
anilkalburagi <anilkalburagi1999.com>
2022-09-27 22:28:26 +0530

    progarm for datatype

---
+++ datatype/bike.java
@@ -0,0 +1,34 @@
+class bike{
+		public static void main(String []arg){
+		System.out.println("string on main method");
+		
+		//byte price;
+		int price = 2200;
+		System.out.println(price);
+		
+		char name = 'h';
+		System.out.println(name);
+		
+		boolean battery = false;
+		System.out.println(battery);
+		
+		long year;
+		 year = 28051999;
+		System.out.println(year);
+		
+		short speed = 100;
+		System.out.println(speed);
+		
+		float ratings = 4;
+		System.out.println(ratings);
+		
+		double cc = 125;
+		System.out.println(cc);
+		
+		
+		
+		
+		
+		}
+}
+
---
+++ datatype/data.java
@@ -0,0 +1,34 @@
+class Ani{
+		public static void main(String []arg){
+		System.out.println("string on main method");
+		
+		byte age;
+		age=22;
+		System.out.println(age);
+		
+		char middelname='M';
+		System.out.println(middelname);
+		
+		boolean jobholder = false;
+		System.out.println(jobholder);
+		
+		long DOB;
+		int dob=28051999;
+		System.out.println(dob);
+		
+		short num=100;
+		System.out.println(num);
+		
+		float aggrregate=7.2;
+		System.out.println(aggrregate);
+		
+		double percentage = 65;
+		System.out.println(percentage);
+		
+		
+		
+		
+		
+		}
+}
+
