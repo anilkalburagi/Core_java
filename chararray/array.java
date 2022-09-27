41fe79399a1a2e9f56fe0dd3c25ecaf95bd9eed3
anilkalburagi <anilkalburagi1999.com>
2022-09-27 22:32:13 +0530

    program for charcater_array

---
+++ chararray/array.java
@@ -0,0 +1,14 @@
+class array{
+		public static void main(String []args){
+		char name1='a';
+		char name2='n';
+		char name3='i';
+		char name4='l';
+		char name5='k';
+		char name6='u';
+		char name7='m';
+		char name8='a';
+		char name9='r';
+		System.out.println(name1 +""+ name2 +""+ name3+""+ name4 +""+ name5 +""+ name6 +""+ name7 +""+ name8 +""+ name9);
+		}
+}
---
+++ chararray/gruparray.java
@@ -0,0 +1,24 @@
+class gruparray{
+		public static void main(String []args){
+		char name1='a';
+		char name2='n';
+		char name3='i';
+		char name4='l';
+		char name5='k';
+		char name6='u';
+		char name7='m';
+		char name8='a';
+		char name9='r';
+		
+		
+		System.out.println(name1 +""+ name2 +""+ name3+""+ name4 +""+ name5 +""+ name6 +""+ name7 +""+ name8 +""+ name9);
+		
+		char name[] = {'M','a','r','g','o','l'};
+		
+		System.out.println(name);
+		
+		System.out.println(name[2]);
+		float cgpa[]={5.0f,4.9f,4.8f,4.7f,4.6f,4.5f};
+		System.out.println(cgpa);
+		}
+}
