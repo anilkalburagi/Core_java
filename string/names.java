cfc66f6bf8a0bc3fe18237369e6b67e562c872be
anilkalburagi <anilkalburagi1999.com>
2022-09-28 14:16:37 +0530

    program for string

---
+++ string/names.java
@@ -0,0 +1,9 @@
+class name{
+	public static void main(String ak[]){
+	String names="Anil kumar MM";
+	System.out.println(names);
+	
+	int noofchar = names.length();
+		System.out.println("size of string" + noofchar);
+	}
+}
---
+++ string/zoo.java
@@ -0,0 +1,9 @@
+class zoo{
+	public static void main(String []ak){
+		String animals_name = "tiger,lion,monkey,camel";
+		System.out.println(animals_name);
+		
+		int noofchar = animals_name.length();
+		System.out.println("size of char" + noofchar);
+	}
+}
