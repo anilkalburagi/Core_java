56bdea4f3515388a67d5c3f233637debc5f5c6d8
anilkalburagi <anilkalburagi1999.com>
2022-09-28 21:51:33 +0530

    code for constracuter

---
+++ constracuter/Bike.java
@@ -0,0 +1,12 @@
+class Bike{
+	 String brandName;
+	 char color;
+	 byte noofTyer;
+	 int price;
+	 
+	 Bike (){
+		System.out.println("creating object");
+		brandName = "Hero";
+		System.out.println("object created");
+	 }
+}
---
+++ constructer/BikeTester.java
@@ -0,0 +1,13 @@
+class BikeTester{
+	public static void main(String []ak){
+	 Bike bike = new Bike();
+	 
+	 System.out.println(bike);
+	 System.out.println(bike.brandName);
+	 
+	 Bike bike1 = new Bike();
+	 
+	 System.out.println(bike1);
+	 System.out.println(bike1.brandName);
+	}
+}
