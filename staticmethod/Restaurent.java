5fd054d0da0c780a00375d8aad156631a2dfc9e9
anilkalburagi <anilkalburagi1999.com>
2022-09-30 18:05:07 +0530

    code for static method

---
+++ staticmethod/Restaurent.java
@@ -0,0 +1,15 @@
+class Restaurent{
+	static String name = "Annapuranna";
+	static short noofitems = 200;
+	
+	String location = "kalburagi";
+	byte noofemployee = 45;
+	float reviews = 3.5f;
+	int cost = 2;
+	boolean isPuerVEG = true;
+	String owner = ("A"+"n"+"i");
+	String partners = "prajwal sachin sunil bhagy" ;
+	
+	
+	
+}
---
+++ staticmethod/Testrestaurent.java
@@ -0,0 +1,16 @@
+class Testrestaurent {
+	public static void main(String []ak){
+	System.out.println(Restaurent.name);
+	System.out.println(Restaurent.noofitems);
+	
+	Restaurent annapuranna = new Restaurent();
+	System.out.println(annapuranna.location);
+	System.out.println(annapuranna.noofemployee);
+	System.out.println(annapuranna.reviews);
+	System.out.println(annapuranna.cost);
+	System.out.println(annapuranna.isPuerVEG);
+	System.out.println(annapuranna.owner);
+	System.out.println(annapuranna.partners);
+	
+	}
+}
